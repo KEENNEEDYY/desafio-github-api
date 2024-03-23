d0b4152eddd4391567b6920bf505bf7b46b44795
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2024-03-23 17:15:46 -0300

    Adding React Router DOM
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 
 export default function App() {
-
   return (
-    <h1>Hello friend</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route index element={
+          <>
+            <h1>hello friend</h1>
+            <h3>Home page</h3>
+          </>
+        } />
+      </Routes>
+    </BrowserRouter>
+  );
 }
