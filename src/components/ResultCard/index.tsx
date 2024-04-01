5b2096a3ba1b210f3d27ebf983afd35b927f77f4
KEENNEEDYY <kennedy.costa.valenet@gmail.com>
2024-04-01 07:32:59 -0300

    Result card responsive
    Signed-off-by: KEENNEEDYY <kennedy.costa.valenet@gmail.com>

---
+++ src/components/ResultCard/index.tsx
@@ -0,0 +1,37 @@
+
+import './styles.css';
+
+type Props = {
+    avatar_url: string;
+    perfil_url: string;
+    followers: string;
+    location: string;
+    name: string;
+}
+
+export default function ResultCard(
+    /* {
+        avatar_url,perfil_url,followers,location,name
+    } : Props */
+) {
+    return (
+        <div className="gitapi-profile-content">
+            <img src="https://avatars.githubusercontent.com/u/13897257?v=4" alt="perfil-image" />
+            <div className="gitapi-profile-data">
+                <h4>Informações</h4>
+                <div className="gitapi-profile-data-item gitapi-url">
+                    <h5>Perfil: </h5><p>https://api.github.com/users/acenelio</p>
+                </div>
+                <div className="gitapi-profile-data-item">
+                    <h5>Seguidores: </h5><h6>4379</h6>
+                </div>
+                <div className="gitapi-profile-data-item">
+                    <h5>Localidade: </h5><h6>Uberlândia</h6>
+                </div>
+                <div className="gitapi-profile-data-item">
+                    <h5>Nome: </h5><h6>Nelio Alves</h6>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/routes/ProfilePresenter/index.tsx
+++ src/routes/ProfilePresenter/index.tsx
@@ -1,8 +1,25 @@
 
+import Button from '../../components/Button';
+import ResultCard from '../../components/ResultCard';
+import './styles.css';
+
 export default function ProfilePresenter() {
+
+
     return (
-        <div>
-            <h1>Tela de busca de perfil Github</h1>
-        </div>
+        <main>
+            <section>
+                <form className="gitapi-profile-presenter-container">
+                    <h1>Encontre um perfil Github</h1>
+                    <input
+                        type="text"
+                        name="search"
+                        placeholder="Digite o nome do perfil..."
+                    />
+                    <Button text="Encontrar" />
+                </form>
+                <ResultCard />
+            </section>
+        </main>
     )
 }
